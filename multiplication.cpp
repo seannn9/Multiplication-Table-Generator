b6a084aafb3a666ed690185290668545382c875b
seannn9 <naeskirlu9@gmail.com>
2023-03-11 19:52:52 +0800

    Added input prompt for multiplicand and multiplier

--- multiplication.cpp
+++ multiplication.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 int main() {
 
-    for (int i = 1; i <= 10; i++) {
-        for (int j = 1; j <= 10; j++) {
+    int multiplicand, multiplier;
+    cout << "Enter multiplicand: ";
+    cin >> multiplicand;
+    cout << "Enter multiplier: ";
+    cin >> multiplier;
+
+    for (int i = 1; i <= multiplier; i++) {
+        for (int j = 1; j <= multiplicand; j++) {
             cout << i * j << "\t";
-            if (j == 10) {
+            if (j == multiplicand) {
                 cout << endl;
             }            
         } 
